2d1971801038803246468d2ac9430cd54e14b9d6
Prasad Ravan <prasadcravan@gmail.com>
2022-08-11 23:49:56 +0530

    Insertion Sort

---
+++ AlgorithmTests/InsertionSortTests.cs
@@ -0,0 +1,52 @@
+﻿namespace AlgorithmTests
+{
+    using InsertionSort;
+    using NUnit.Framework;
+    using System;
+
+    internal class InsertionSortTests
+    {
+        [Test]
+        public void InsertionSortValidInputs()
+        {
+            RunTestsForInsertionSort(InsertionSort.InsertionSortAlgorithm);
+        }
+
+        private void RunTestsForInsertionSort(Func<int[], int[]> insertionSortAlgorithm)
+        {
+            var sampleData = GetSampleData();
+
+            foreach (var sample in sampleData)
+            {
+                var result = insertionSortAlgorithm(sample);
+                CollectionAssert.IsOrdered(result);
+                PrintResults(result);
+            }
+        }
+
+        private void PrintResults(int[] result)
+        {
+            TestContext.WriteLine("---------------------------------------");
+
+            foreach (var num in result)
+            {
+                TestContext.Write(num + " ");
+            }
+
+            TestContext.WriteLine("***************************************");
+        }
+
+        private int[][] GetSampleData()
+        {
+            var sampleData = new int[5][];
+
+            sampleData[0] = new int[] { 3, 1 };
+            sampleData[1] = new int[] { 9, 5, 8, -1, 4 };
+            sampleData[2] = new int[] { 1, 2, 3, 4, 5, 6 };
+            sampleData[3] = new int[] { -1 };
+            sampleData[4] = new int[] { 5, 0, 6, 7, 8, 9, 2 };
+
+            return sampleData;
+        }
+    }
+}
---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,37 @@
+﻿namespace InsertionSort
+{
+    public class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 9, 5, 8, -1, 4 };
+
+            InsertionSortAlgorithm(array);
+
+            foreach (var num in array)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int[] InsertionSortAlgorithm(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                var firstUnsorted = nums[i];
+                int j;
+
+                for (j = i; j > 0 && nums[j - 1] > firstUnsorted; j--)
+                {
+                    nums[j] = nums[j - 1];
+                }
+
+                nums[j] = firstUnsorted;
+            }
+
+            return nums;
+        }
+    }
+}
