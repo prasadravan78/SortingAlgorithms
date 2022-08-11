d8a1932af8c91efc22feafbe19dbf8a68d07b41d
Prasad Ravan <prasadcravan@gmail.com>
2022-08-12 00:32:39 +0530

    Shell Sort

---
+++ AlgorithmTests/ShellSortTests.cs
@@ -0,0 +1,52 @@
+﻿namespace AlgorithmTests
+{
+    using ShellSort;
+    using NUnit.Framework;
+    using System;
+
+    internal class ShellSortTests
+    {
+        [Test]
+        public void ShellSortValidInputs()
+        {
+            RunTestsForShellSort(ShellSort.ShellSortAlgorithm);
+        }
+
+        private void RunTestsForShellSort(Func<int[], int[]> shellSortAlgorithm)
+        {
+            var sampleData = GetSampleData();
+
+            foreach (var sample in sampleData)
+            {
+                var result = shellSortAlgorithm(sample);
+                CollectionAssert.IsOrdered(result);
+                PrintResults(result);
+            }
+        }
+
+        private void PrintResults(int[] result)
+        {
+            TestContext.WriteLine("---------------------------------------");
+
+            foreach (var num in result)
+            {
+                TestContext.Write(num + " ");
+            }
+
+            TestContext.WriteLine("***************************************");
+        }
+
+        private int[][] GetSampleData()
+        {
+            var sampleData = new int[5][];
+
+            sampleData[0] = new int[] { 3, 1 };
+            sampleData[1] = new int[] { 9, 5, 8, -1, 4 };
+            sampleData[2] = new int[] { 1, 2, 3, 4, 5, 6 };
+            sampleData[3] = new int[] { -1 };
+            sampleData[4] = new int[] { 5, 0, 6, 7, 8, 9, 2 };
+
+            return sampleData;
+        }
+    }
+}
---
+++ ShellSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace ShellSort
+{
+    public class ShellSort
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 3, 6, 2, 9, 8 };
+
+            var result = ShellSortAlgorithm(array);
+
+            foreach (var num in result)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static int[] ShellSortAlgorithm(int[] array)
+        {
+            int gap = 1;
+
+            while (gap < array.Length / 3)
+            {
+                gap = 3 * gap + 1;
+            }
+
+            while (gap >= 1)
+            {
+                for (int i = gap; i < array.Length; i++)
+                {
+                    for (int j = i; j >= gap && array[j] < array[j - gap]; j = j - gap)
+                    {
+                        (array[j], array[j - gap]) = (array[j - 1], array[j]);
+                    }
+                }
+
+                gap = gap / 3;
+            }
+
+            return array;
+        }
+    }
+}
