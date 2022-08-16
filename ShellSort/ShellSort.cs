afd688811abf0942ee0767fa75eb56efeaa9177e
Prasad Ravan <prasadcravan@gmail.com>
2022-08-16 18:40:39 +0530

    Code formatting style cop changes

--- ShellSort/ShellSort.cs
+++ ShellSort/ShellSort.cs
@@ -1,10 +1,14 @@
-﻿using System.Globalization;
-
-namespace ShellSort
+﻿namespace ShellSort
 {
+    /// <summary>
+    /// Provides methods for shell sort.
+    /// </summary>
     public class ShellSort
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Entry point for Program.
+        /// </summary>
+        public static void Main()
         {
             var array = new int[] { 3, 6, 2, 9, 8 };
 
@@ -18,26 +22,31 @@ namespace ShellSort
             Console.ReadLine();
         }
 
+        /// <summary>
+        /// Performs shell sort.
+        /// </summary>
+        /// <param name="array">Input array elemet.</param>
+        /// <returns>Sorted Array.</returns>
         public static int[] ShellSortAlgorithm(int[] array)
         {
             int gap = 1;
 
             while (gap < array.Length / 3)
             {
-                gap = 3 * gap + 1;
+                gap = (3 * gap) + 1;
             }
 
             while (gap >= 1)
             {
                 for (int i = gap; i < array.Length; i++)
                 {
-                    for (int j = i; j >= gap && array[j] < array[j - gap]; j = j - gap)
+                    for (int j = i; j >= gap && array[j] < array[j - gap]; j -= gap)
                     {
                         (array[j], array[j - gap]) = (array[j - 1], array[j]);
                     }
                 }
 
-                gap = gap / 3;
+                gap /= 3;
             }
 
             return array;
