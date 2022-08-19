4aa33cefbcb89058edfcb6a7bd114747a693ac02
Prasad Ravan <prasadcravan@gmail.com>
2022-08-20 00:47:06 +0530

    Quick Sort

---
+++ AlgorithmTests/QuickSortTests.cs
@@ -0,0 +1,57 @@
+﻿namespace AlgorithmTests
+{
+    using NUnit.Framework;
+    using QuickSort;
+
+    /// <summary>
+    /// Provides Quick sort tests.
+    /// </summary>
+    public class QuickSortTests
+    {
+        /// <summary>
+        /// Calls Quick sort with valid inputs.
+        /// </summary>
+        [Test]
+        public void QuickSortValidInput()
+        {
+            RunTestsForQuickSort(QuickSort.QuickSortAlgorithm);
+        }
+
+        private static int[][] GetSampleData()
+        {
+            var sampleData = new int[5][];
+
+            sampleData[0] = new int[] { 3, 1 };
+            sampleData[1] = new int[] { 9, 5, 8, -1, 4 };
+            sampleData[2] = new int[] { 1, 2, 3, 4, 5, 6 };
+            sampleData[3] = new int[] { -1 };
+            sampleData[4] = new int[] { 5, 0, 6, 7, 8, 9, 2 };
+
+            return sampleData;
+        }
+
+        private static void RunTestsForQuickSort(Action<int[]> quickSortAlgorithm)
+        {
+            var sampleData = GetSampleData();
+
+            foreach (var sample in sampleData)
+            {
+                quickSortAlgorithm(sample);
+                CollectionAssert.IsOrdered(sample);
+                PrintResults(sample);
+            }
+        }
+
+        private static void PrintResults(int[] sample)
+        {
+            TestContext.WriteLine("---------------------------------------");
+
+            foreach (var num in sample)
+            {
+                TestContext.Write(num + " ");
+            }
+
+            TestContext.WriteLine("***************************************");
+        }
+    }
+}
---
+++ QuickSort/QuickSort.cs
@@ -0,0 +1,61 @@
+﻿namespace QuickSort
+{
+    public class QuickSort
+    {
+        static void Main()
+        {
+            var array = new int[] { 3, 6, 2, 9, 8 };
+
+            QuickSortAlgorithm(array);
+
+            foreach (var num in array)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void QuickSortAlgorithm(int[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort(int[] array, int start, int end)
+        {
+            if (start < end)
+            {
+                int partitionIndex = Partition(array, start, end);
+
+                if (partitionIndex > 1)
+                {
+                    Sort(array, start, partitionIndex - 1);
+                }
+
+                if (partitionIndex + 1 < end)
+                {
+                    Sort(array, partitionIndex + 1, end);
+                }
+            }
+        }
+
+        private static int Partition(int[] array, int start, int end)
+        {
+            int pivot = array[start];
+            int startIndex = start;
+
+            for (int i = start + 1; i <= end; i++)
+            {
+                if (array[i] < pivot)
+                {
+                    startIndex++;
+                    (array[startIndex], array[i]) = (array[i], array[startIndex]);
+                }
+            }
+
+            (array[start], array[startIndex]) = (array[startIndex], array[start]);
+
+            return startIndex;
+        }
+    }
+}
